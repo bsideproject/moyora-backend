978464680d8e022832506a7195cf29ee8f466adb
kimdeiz <kimdeiz@naver.com>
2022-11-23 22:59:12 +0900

    initial commit

---
+++ src/main/java/com/beside/ties/TiesApplication.java
@@ -0,0 +1,13 @@
+package com.beside.ties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beside/ties/TiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beside.ties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
