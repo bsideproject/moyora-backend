a3b4232c802a399187def09490ca1b4153265030
kimdeiz <kimdeiz@naver.com>
2022-12-04 00:05:55 +0900

    fix login

---
+++ src/main/java/com/beside/ties/dto/user/response/LoginResponseDto.java
@@ -0,0 +1,13 @@
+package com.beside.ties.dto.user.response;
+
+import com.beside.ties.auth.kakao.KakaoToken;
+import lombok.Data;
+
+@Data
+public class LoginResponseDto {
+
+    public final String email;
+    public final String profileImageUrl;
+    public final String nickname;
+    private final String name;
+}
