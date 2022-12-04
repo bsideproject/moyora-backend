ab05bfdebcecdda354403d5cef6258973ac291b5
zooragi <eldehd9898@gmail.com>
2022-12-04 14:21:44 +0900

    Fix service.sh jar file name

--- service.sh
+++ service.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SERVICE_NAME=ties-main
-PATH_TO_JAR=./gitaction-0.0.1-SNAPSHOT.jar
+PATH_TO_JAR=./ties-0.0.1-SNAPSHOT.jar
 PID_PATH_NAME=/tmp/$SERVICE_NAME-pid # pid name
 RUN_MODE=sendbox
 case $1 in
