a49fee3813fb7f61387c84980f080a649f058408
zooragi <eldehd9898@gmail.com>
2022-12-18 19:41:09 +0900

    add SchoolGuestBook entity

---
+++ src/main/java/com/beside/ties/domain/schoolguestbook/entity/SchoolGuestBook.java
@@ -0,0 +1,31 @@
+package com.beside.ties.domain.schoolguestbook.entity;
+
+import com.beside.ties.domain.account.entity.Account;
+import com.beside.ties.domain.school.entity.School;
+import com.beside.ties.global.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SchoolGuestBook extends BaseTimeEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "sgb_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "school_id")
+    private School school;
+
+    @OneToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    private String content;
+}
