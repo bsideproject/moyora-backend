a10765be201a7d9bfdb88057245c482ea22ab4fa
zooragi <eldehd9898@gmail.com>
2023-01-06 11:46:58 +0900

    fix : Modify git action

--- service.sh
+++ service.sh
@@ -7,7 +7,9 @@ case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar -Dspring.profiles.active=$RUN_MODE $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+            nohup java -jar \
+                  -Dspring.config.location=classpath:/application.yml,/home/app/application-secret.yml,classpath:/application-jpa.yml \
+                  -Dspring.profiles.active=$RUN_MODE $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
             echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
@@ -33,7 +35,9 @@ case $1 in
             echo "$SERVICE_NAME stopped ...";
             rm $PID_PATH_NAME
             echo "$SERVICE_NAME starting ..."
-            nohup java -jar -Dspring.profiles.active=$RUN_MODE $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+            nohup java -jar \
+                  -Dspring.config.location=classpath:/application.yml,/home/app/application-secret.yml,classpath:/application-jpa.yml \
+                  -Dspring.profiles.active=$RUN_MODE $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
             echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
