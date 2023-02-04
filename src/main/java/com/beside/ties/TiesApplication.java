38f6663d4fb69ae099600952f8011c7cd4d8cae6
zooragi <eldehd9898@gmail.com>
2023-02-04 22:23:16 +0900

    cors 설정

--- src/main/java/com/beside/ties/TiesApplication.java
+++ src/main/java/com/beside/ties/TiesApplication.java
@@ -11,7 +11,10 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -24,4 +27,18 @@ public class TiesApplication {
 		SpringApplication.run(TiesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+						.allowedOrigins("https://www.moyorafriends.co.kr/")
+						.allowedOrigins("http://localhost:3000")
+						.allowedOrigins("https://localhost:3000");
+			}
+		};
+	}
 }
