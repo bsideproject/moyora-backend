367f32ef4dbc0ae000750140ec9427bd2fb10a58
zooragi <eldehd9898@gmail.com>
2023-02-06 01:10:30 +0900

    통계 데이터가 없으면 0반환 수정

--- src/main/java/com/beside/ties/domain/schoolregion/service/SchoolRegionService.java
+++ src/main/java/com/beside/ties/domain/schoolregion/service/SchoolRegionService.java
@@ -56,7 +56,8 @@ public class SchoolRegionService {
                 .map(item -> percentCalculate(item.getCount(), totalCount))
                 .collect(Collectors.toList());
 
-        top5Percent.add(100L-top5Percent.stream().mapToLong(Long::longValue).sum());
+        top5Percent.add(100L-top5Percent.stream().mapToLong(Long::longValue).sum() == 100L ?
+                0L : 100L-top5Percent.stream().mapToLong(Long::longValue).sum());
 
         return top5Percent;
     }
