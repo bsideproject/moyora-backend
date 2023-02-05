f636bce19ae20c3ee01bdc3eed02144fc1a36963
kimdeiz <kimdeiz@naver.com>
2023-02-06 01:50:07 +0900

    mbti 수정시 count 로직 추가

--- src/main/java/com/beside/ties/domain/schoolmbti/service/SchoolMbtiService.java
+++ src/main/java/com/beside/ties/domain/schoolmbti/service/SchoolMbtiService.java
@@ -1,6 +1,12 @@
 package com.beside.ties.domain.schoolmbti.service;
 
+import com.beside.ties.domain.account.entity.MBTI;
 import com.beside.ties.domain.common.dto.StatisticsDto;
+import com.beside.ties.domain.jobcategory.entity.JobCategory;
+import com.beside.ties.domain.mbti.entity.Mbti;
+import com.beside.ties.domain.mbti.repo.MbtiRepo;
+import com.beside.ties.domain.school.entity.School;
+import com.beside.ties.domain.schooljob.entity.SchoolJob;
 import com.beside.ties.domain.schoolmbti.entity.SchoolMbti;
 import com.beside.ties.domain.schoolmbti.repo.SchoolMbtiRepo;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -16,6 +23,7 @@ import java.util.stream.Collectors;
 public class SchoolMbtiService {
 
     private final SchoolMbtiRepo schoolMbtiRepo;
+    private final MbtiRepo mbtiRepo;
 
     public List<SchoolMbti> countTop4BySchoolId(Long schoolId) {
         return schoolMbtiRepo.findTop4BySchool_IdOrderByCountDesc(schoolId);
@@ -42,6 +50,30 @@ public class SchoolMbtiService {
     public void countPlusOneUpdate(SchoolMbti schoolMbti) {
         schoolMbti.plusOneCount();
     }
+
+    public void countPlus(School school, String stringMbti) {
+        Mbti mbti = mbtiRepo.findByName(stringMbti).orElseThrow(() -> new IllegalArgumentException("해당 엠비티아이가 존재하지 않습니다."));
+
+        Optional<SchoolMbti> optionalSchoolMbti = schoolMbtiRepo.findBySchool_IdAndMbti_Id(school.getId(), mbti.getId());
+        if(optionalSchoolMbti.isPresent()){
+            optionalSchoolMbti.get().plusOneCount();
+        }else{
+                schoolMbtiRepo.save(new SchoolMbti(mbti, school));
+        }
+    }
+
+    public void countMinus(School school, String stringMbti) {
+        Mbti mbti = mbtiRepo.findByName(stringMbti).orElseThrow(() -> new IllegalArgumentException("해당 엠비티아이가 존재하지 않습니다."));
+
+        Optional<SchoolMbti> optionalSchoolMbti = schoolMbtiRepo.findBySchool_IdAndMbti_Id(school.getId(), mbti.getId());
+        if(optionalSchoolMbti.isPresent()){
+            optionalSchoolMbti.get().minusOneCount();
+        }else{
+            schoolMbtiRepo.save(new SchoolMbti(mbti, school));
+        }
+    }
+
+
 
     public List<StatisticsDto> convertStatisticsDto(List<SchoolMbti> schoolMbties, Long totalCount) {
         return schoolMbties.stream()
